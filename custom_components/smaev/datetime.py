2193fe45bb3315087c19ec136ba9a08bf64b40a7
Andre Lengwenus <alengwenus@gmail.com>
2024-02-11 13:24:24 +0100

    Forced refresh task is handled in correct event loop (#22)

--- custom_components/smaev/datetime.py
+++ custom_components/smaev/datetime.py
@@ -67,7 +67,7 @@ async def async_setup_entry(
     for entity_description in DATETIME_DESCRIPTIONS:
         entities.append(
             SmaEvChargerDateTime(
-                coordinator, config_entry.unique_id, device_info, entity_description
+                hass, coordinator, config_entry, device_info, entity_description
             )
         )
 
@@ -82,17 +82,19 @@ class SmaEvChargerDateTime(CoordinatorEntity, DateTimeEntity):
 
     def __init__(
         self,
+        hass: HomeAssistant,
         coordinator: DataUpdateCoordinator,
-        config_entry_unique_id: str,
+        config_entry: ConfigEntry,
         device_info: DeviceInfo,
         entity_description: SmaEvChargerDateTimeEntityDescription,
     ) -> None:
         """Initialize the sensor."""
         super().__init__(coordinator)
+        self.hass = hass
         self.entity_description = entity_description
 
         self._attr_device_info = device_info
-        self._attr_unique_id = f"{config_entry_unique_id}-{self.entity_description.key}"
+        self._attr_unique_id = f"{config_entry.unique_id}-{self.entity_description.key}"
         self._attr_native_value = None
 
     @callback
